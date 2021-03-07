71d3d620dabb52a69fa2479621976bf2eca7f767
Ferid Eyvazov <ferideyvazov25@gmail.com>
2021-03-07 22:26:19 +0400

    updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,17 +9,13 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-from decouple import config
 from pathlib import Path
 import django_heroku
 import cloudinary_storage
 
-#### FOR .ENV #####
-# import os
-# from dotenv import load_dotenv
-# load_dotenv()
-
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
 
@@ -31,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -185,8 +181,8 @@ AUTH_USER_MODEL = 'jobs.CustomUser'
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'dcnbcvr2z', 
-    'API_KEY': config('API_KEY'),
-    'API_SECRET': config('API_SECRET'),
+    'API_KEY': '692917715259323',
+    'API_SECRET': 'Go_qf7Ng4cTvXr5EetqgffEKt4k',
 }
 
 
